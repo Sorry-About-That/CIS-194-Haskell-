da5fbaa634df6c75a02c90575a0a2c47df5914bd
Sorry-About-That <marcmarkabawi100@gmail.com>
2024-12-01 18:09:04 -0500

    Create creditcardRefactored.hs
    Refactored version of my original beginner solution. This is still a beginner solution though.

---
+++ HW1/creditcardRefactored.hs
@@ -0,0 +1,35 @@
+-- CIS HW 1
+
+    --Goal here is to make a function that takes a number, and splits it into a list containing the digits
+toDigits :: Integer -> [Integer]
+toDigits n
+    | n <= 0 = []
+    | otherwise = map (\c -> read [c]) $ show n
+    -- Goal is same as above, but then reverse the digits 
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse (toDigits n)
+
+--Goal here is to double every second value, so second, fourth, etc.
+      -- doubleEveryOtherRight is the final implementation
+doubleEveryOtherRight :: [Integer] -> [Integer]
+doubleEveryOtherRight n = reverse (doubleEveryOther (reverse n))
+
+         -- Helper funnction
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:y:zs) = x : (2 * y) : doubleEveryOther zs
+
+
+    --Goal here is to sum the *digits* present in the list
+
+sumDigits :: [Integer] -> Integer
+sumDigits []     = 0
+sumDigits (x:xs) = (sum $ toDigits x) + sumDigits xs
+
+   
+validate :: Integer -> Bool
+validate n = intermediate `mod` 10 == 0 
+    where 
+        intermediate =
+        sumDigits (doubleEveryOtherRight (toDigits n))
