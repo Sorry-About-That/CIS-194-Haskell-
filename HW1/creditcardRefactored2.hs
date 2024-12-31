0423b3b2fa0cbc6fef7453481504e4098b32cdd1
Sorry-About-That <marcmarkabawi100@gmail.com>
2024-12-31 16:42:20 -0700

    Create creditcardRefactored2.hs

---
+++ HW1/creditcardRefactored2.hs
@@ -0,0 +1,30 @@
+-- CIS HW 1
+
+    --Goal here is to make a function that takes a number, and splits it into a list containing the digits
+toDigits :: Integer -> [Integer]
+toDigits n
+    | n <= 0    = []
+    | otherwise = map (\c -> read [c]) $ show n
+    -- Goal is same as above, but then reverse the digits 
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev = reverse . toDigits
+
+doubleEveryOtherRig :: [Integer] -> [Integer]
+doubleEveryOtherRig xs = 
+ if even (length xs)
+  then zipWith (\ x y -> (if odd y then 2 * x else x)) xs [1..length xs]
+  else zipWith (\ x y -> (if even y then 2 * x else x)) xs [1..length xs]
+
+
+    --Goal here is to sum the *digits* present in the list
+
+sumDigits :: [Integer] -> Integer
+sumDigits []     = 0
+sumDigits (x:xs) = (sum $ toDigits x) + sumDigits xs
+
+   
+validate :: Integer -> Bool
+validate n = intermediate `mod` 10 == 0 
+    where 
+        intermediate =
+        (sumDigits.doubleEveryOtherRight.toDigits) n
